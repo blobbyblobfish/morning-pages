5eb6e208b074aae12a78cadc3a1992e37437775a
blobbyblobfish <aywang6@gmail.com>
2020-09-03 14:41:34 -0400

    StyleSheet

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { Text, View } from 'react-native'
+import { styles } from './StyleSheet.js'
 
 export default function App() {
   return (
@@ -8,14 +9,6 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
